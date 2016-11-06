e0f6a87b3901e27f70d0c08b3e2d04eeecb87f19
Priya Ranjan Singh <mail@priyaranjan.net>
2016-11-06 19:37:14 +0530

    add markup and styles for forecast blocks

--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -19,17 +19,38 @@ export default class Weather extends React.Component {
   render() {
     return (
       <div>
-        <div>Weather</div>
         <form>
-          <label htmlFor="city" />
-          <input id="city" onChange={this.handleChange.bind(this)} />
+          <div className="form-group">
+            <label htmlFor="city" className="form-control">City</label>
+            <input id="city"
+                   onChange={this.handleChange.bind(this)}
+                   className="form-control"
+                   placeholder="Enter city" />
+          </div>
           <button onClick={this.handleClick.bind(this)}
                   disabled={!this.state.city}>
             Get weather forecast
           </button>
         </form>
-        <div>{ this.state.weather && this.state.weather.city }</div>
-        <div>{ JSON.stringify(this.state.weather && this.state.weather.forecast[0]) }</div>
+        { this.state.weather && this.state.weather.city &&
+          <div className="city">
+            <span>Showing weather for </span>
+            <span className="city-name">{ this.state.weather && this.state.weather.city }</span>
+          </div>
+        }
+        <div className="forecast-container">
+        {
+          this.state.weather && this.state.weather.forecast.map((forecast, index) => {
+            return (
+              <div key={index} className="forecast">
+                <div>{ this.getDate(forecast.dt_txt) }</div>
+                <div>{ forecast.weather[0]['main'] }</div>
+                <div>{ forecast.main.temp } F</div>
+              </div>
+            );
+          })
+        }
+        </div>
       </div>
     );
   }
@@ -51,5 +72,9 @@ export default class Weather extends React.Component {
   handleClick(event) {
     this.setWeather(this.state.city);
     event.preventDefault();
+  }
+
+  getDate(t) {
+    return new Date(t).toDateString();
   }
 }
